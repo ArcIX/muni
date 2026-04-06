3b94faff29ea8496d123a3e59d74bb2b80db057c
ArcIX <rc2582739@gmail.com>
2026-04-06 16:52:46 +0800

    feat(bigquery_provider): update `get_data()` to include strategy parameter and execute query

--- src/muni/providers/bigquery_provider.py
+++ src/muni/providers/bigquery_provider.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from google.cloud import bigquery
+from muni.strategies import BaseStrategy
 from muni.providers import BaseProvider
 
 class BigQueryProvider(BaseProvider):
@@ -7,5 +8,9 @@ class BigQueryProvider(BaseProvider):
         self.client = client
         self.dataset_name = dataset_name
 
-    def get_data(self, ticker: str, start_date: str, end_date: str) -> pd.DataFrame:
-        return pd.DataFrame()
+    def get_data(
+        self, ticker: str, start_date: str, end_date: str, strategy: BaseStrategy
+    ) -> pd.DataFrame:
+        query_str = strategy.get_sql_query_string(ticker, start_date, end_date)
+        results_df = self.client.query(query_str).to_dataframe()
+        return results_df
