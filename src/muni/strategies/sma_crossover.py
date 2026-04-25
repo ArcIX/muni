5c132af56f27737216ffa7d0d3dea1e4058f5b09
ArcIX <rc2582739@gmail.com>
2026-04-25 13:50:31 +0800

    feat(strategies/sma): add SQL query string generation for signal calculation in `SMACrossover`

--- src/muni/strategies/sma_crossover.py
+++ src/muni/strategies/sma_crossover.py
@@ -17,3 +17,48 @@ class SMACrossover(BaseStrategy):
         # Shift to avoid Lookahead Bias
         data_df["signal"] = raw_signal.shift(1).fillna(0)
         return data_df
+    
+    def get_sql_query_string(self, ticker: str, start_date: str, end_date: str) -> str:
+        return f"""
+            WITH indicators AS (
+                SELECT
+                    trade_date,
+                    ticker,
+                    adj_close,
+                    -- Calculate short and long term averages
+                    AVG(adj_close) OVER(fast_window) AS fast_sma,
+                    AVG(adj_close) OVER(slow_window) AS slow_sma
+                FROM `test_dataset.test_table`
+                WHERE 
+                    ticker = '{ticker}' AND 
+                    trade_date BETWEEN '{start_date}' AND '{end_date}'
+                WINDOW 
+                    fast_window AS (
+                        PARTITION BY ticker
+                        ORDER BY trade_date
+                        ROWS BETWEEN {self.fast_window - 1} PRECEDING AND CURRENT ROW
+                    ),
+                    slow_window AS (
+                        PARTITION BY ticker
+                        ORDER BY trade_date
+                        ROWS BETWEEN {self.slow_window - 1} PRECEDING AND CURRENT ROW
+                    )
+            ),
+            raw_signals AS (
+                SELECT
+                    *,
+                    -- Signal Generation Logic
+                    -- Calculate the "State" (1 if fast > slow)
+                    CASE
+                    WHEN fast_sma > slow_sma THEN 1
+                    ELSE 0
+                    END AS raw_signal
+                FROM indicators
+            )
+            SELECT
+            *,
+            -- Shift the signal by 1 day to match Python's .shift(1)
+            -- This ensures 'today's' signal is actually 'yesterday's' math
+            LAG(raw_signal) OVER(PARTITION BY ticker ORDER BY trade_date) AS bq_signal
+            FROM raw_signals;
+        """
